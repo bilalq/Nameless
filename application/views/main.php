ea778cc4aaaa56da12d9a2a26d23e5ed96c84b59
Bilal Quadri <bilalquadri92@gmail.com>
2012-05-19 17:19:44 -0400

    Fixed references

--- application/views/main.php
+++ application/views/main.php
@@ -2,11 +2,11 @@
 <html>
 <head>
   <meta http-equiv="content-type" content="text/html; charset=utf-8">
-  <script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
-  <script type="text/javascript" src="js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="js/main.js"></script>
-  <link rel="stylesheet" href="css/main.css" type="text/css" charset="utf-8">
-  <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" charset="utf-8">
+  <script type="text/javascript" src="/js/jquery-1.7.2.min.js"></script>
+  <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="/js/main.js"></script>
+  <link rel="stylesheet" href="/css/main.css" type="text/css" charset="utf-8">
+  <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css" charset="utf-8">
   <title>No Name</title>
 </head>
 <body>
