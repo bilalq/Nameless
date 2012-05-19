5852af2b83f44a5cf72a863e091ad3166c1f6ba3
Bilal Quadri <bilalquadri92@gmail.com>
2012-05-19 15:45:50 -0400

    Set up placeholder files

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+  <script type="text/javascript" src="js/main.js"></script>
+  <link rel="stylesheet" href="css/main.css" type="text/css" charset="utf-8">
+  <title>No Name</title>
+</head>
+<body>
+  <div id="wrapper">
+    <p>Placeholder</p> 
+  </div>
+</body>
+</html>
