48e19bf0aee2b2f7072073f60b42157dcafbbe74
Bilal Quadri <bilalquadri92@gmail.com>
2012-05-19 16:33:41 -0400

    Added bootstrap

--- index.php
+++ index.php
@@ -3,13 +3,39 @@
 <head>
   <meta http-equiv="content-type" content="text/html; charset=utf-8">
   <script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
   <script type="text/javascript" src="js/main.js"></script>
   <link rel="stylesheet" href="css/main.css" type="text/css" charset="utf-8">
+  <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" charset="utf-8">
   <title>No Name</title>
 </head>
 <body>
   <div id="wrapper">
-    <p>Placeholder</p> 
+
+    <div id="search">
+    </div>
+
+    <div id="definition">
+    </div>
+
+    <div id="synonyms">
+    </div>
+
+    <div id="rhymes">
+    </div>
+
+    <div id="notes">
+    </div>
+
+    <div id="random">
+    </div>
+
+    <div id="pastSearches">
+    </div>
+
+    <div id="domains">
+    </div>
+
   </div>
 </body>
 </html>
