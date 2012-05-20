1a5c0d38d209522779e7e2cb41aff3bc4971192c
Karina Ruzinov <kruzinova@gmail.com>
2012-05-19 22:34:48 -0700

    port

--- public/js/main.js
+++ public/js/main.js
@@ -10,10 +10,16 @@ $(document).ready(function(){
     $.ajax({
       type: 'GET',
       dataType: 'jsonp',
-      jsonp:'jsonp',
+      jsonp: 'jsonp',
       url: (portURL + word),
       success: function(response){
-       console.log("port: " + response);
+        var port = '';
+        for(var i = 0; i < response.length && i < 15; i++){
+          var parsePort = response[i].combined.split(',')[0];
+          port+= '<p>' + parsePort + '</p>';
+          }
+        $('div#combined div').html(port);
+      console.log("port: " + JSON.stringify(response));
       }
     });
 
@@ -24,11 +30,13 @@ $(document).ready(function(){
       url: (rhymeURL + word),
       success: function(response){
        var rhymes = '';
-       for (var i = 0; i < response.length; i++) {
-         rhymes+='<a href="#" class="validWord">'+response[i].word+'</a>';
+       for (var i = 0; i < response.length && i < 15; i++) {
+         if(response[i].score > 260){
+          rhymes+='<a href="#" class="validWord">'+response[i].word+'</a>';
+          }
        };
        console.log("rhyme: " + rhymes);
-
+        $('div#rhymes div').html(rhymes);
       }
     });
   });
