2dca07e2b039cea757d98113ffda6abb87f7d5b1
Karina Ruzinov <kruzinova@gmail.com>
2012-05-20 00:00:10 -0400

    just testing

--- public/js/main.js
+++ public/js/main.js
@@ -7,25 +7,27 @@ $(document).ready(function(){
     event.preventDefault();
     var word = $('form#wordForm input').val();
 
-    //$.ajax({
-      //type: 'GET',
-      //url: 'wordinfo/',
-      //data: ('&word=' + word),
-      //success: function(response){
-        //console.log(response);
-      //}
-    //});
+    $.ajax({
+      type: 'GET',
+      dataType: 'jsonp',
+      jsonp:'jsonp',
+      url: (portURL + word),
+      success: function(response){
+       console.log(response);
+      }
+    });
+
     $.ajax({
       type: 'GET',
       dataType: 'jsonp',
       jsonp: 'jsonp',
       url: (rhymeURL + word),
       success: function(response){
-       var thingy = "";
-       for (var i = 0; i < thingy.length; i++) {
-         '<a href="#">'+thingy[i]+'</a>';
+       var rhymes = '';
+       for (var i = 0; i < response.length; i++) {
+         rhymes+='<a href="#" class="validWord">'+response[i].word+'</a>';
        };
-       console.log("rhyme: " + JSON.stringify(response));
+       console.log("rhyme: " + rhymes);
 
       }
     });
