514055048fe10246fbcc3eb38dd61d8e96fc115d
Karina Ruzinov <kruzinova@gmail.com>
2012-05-19 23:01:23 -0400

    rhyme

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
   var wordForm = $('form#wordForm');
+  var rhymeURL = "http://rhymebrain.com/talk?function=getRhymes&lang=en&maxResults=12&word=";
+  var portURL = "http://rhymebrain.com/talk?function=getPortmanteaus&lang=en&maxResults=12&word=";
 
   wordForm.submit(function(){
     event.preventDefault();
@@ -11,7 +13,16 @@ $(document).ready(function(){
       data: ('&word=' + word),
     });
   });
+
+  $.getJSON("http://rhymebrain.com/talk?function=getRhymes&lang=en&maxResults=12&word="+word+"
+  $.ajax({
+    type: 'GET',
+    dataType: 'jsonp',
+    jsonp: 'jsonp',
+    url: (rhymeURL + word)
+    success: function(response){
+     console.log("rhyme: " + response); 
+    }
+  });
 }); //end docReady
 
-//Rhymes: "http://rhymebrain.com/talk?function=getRhymes&lang=en&maxResults=12&word="+word
-//Ports: "http://rhymebrain.com/talk?function=getPortmanteaus&lang=en&maxResults=12&word=" + word
