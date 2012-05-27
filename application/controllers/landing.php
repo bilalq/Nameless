d47159065a927aac0212039fefbb1d492032571c
Bilal Quadri <bilalquadri92@gmail.com>
2012-05-27 11:58:29 -0700

    Set up skeleton for landing page

---
+++ application/controllers/landing.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Landing extends CI_Controller {
+  public function index()
+  {
+    $this->load->view('landing_page');
+  }
+}
---
+++ application/views/landing_page.php
@@ -0,0 +1,3 @@
+<div id="landing">
+  <p>Testing</p>
+</div>
